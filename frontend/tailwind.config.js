c2a839eb0bc2515b12a7c02e0cf7f03d5b4dbcf0
kaelinwanghu <kaelin.wang.hu@gmail.com>
2025-10-13 22:46:32 -0400

    Finished dependencies and added store and css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        democrat: '#0015BC',
+        republican: '#FF0000',
+        swing: '#9966CC',
+      },
+    },
+  },
+  plugins: [],
+}
